01e301e4d39f3b5fd627a62592942215d8b7a500
Mahadi Hasan Tarunno <47926003+Tarunno@users.noreply.github.com>
2020-10-03 01:02:44 +0600

    Update cart.inc.php

--- includes/cart.inc.php
+++ includes/cart.inc.php
@@ -10,34 +10,15 @@
 		$row = $result->fetch_assoc();
 		$cart->add($_GET['user'],  $_GET['book'],  $row['price'],  $row['name'], $row['image']);
 	}
-	if(isset($_GET['order'])){
-		$result = $book->get_book($_GET['order']);
-		$row = $result->fetch_assoc();
-		$cart->order($_GET['user'],  $_GET['order'],  $row['price'],  $row['name'], $row['image']);
-	}
 	if(isset($_GET['get_cart'])){
 		$result = $cart->get_cart($_GET['get_cart']);
 		$output = '';
 		while($row = $result->fetch_assoc()){
-			$output .= '<div class="item" style="text-align: center; margin-bottom: 10px;">
-				<img style="height: 120px; width:80px; border-radius:4px;" src="assets/book_covers/'.$row['product_image'].'">
+			$output .= '<div class="item" style="text-align: center; margin-bottom: 15px; margin-left: 10px;">
+				<img style="height: 160px; width:120px; border-radius:4px;" src="assets/book_covers/'.$row['product_image'].'">
 				<h4 style="font-size: 14px;"><i class="fas fa-book"></i>  '.$row['product_name'].'</h4>
-				<h4  style="font-size: 13px;"><i class="fas fa-dollar-sign"></i> '.$row['price'].' </h4>
-				<button class="order-btn" onclick="cart_to_order('.$row['product_id'].')"> Order now </button>
-				<button type="button" class="cart-delete-btn" onclick="delete_cart('.$row['id'].')"> <i class="fas fa-trash-alt"></i> </button>
-			</div>';
-		}
-		print_r($output);
-	}
-	if(isset($_GET['get_order'])){
-		$result = $cart->get_order($_GET['get_order']);
-		$output = '';
-		while($row = $result->fetch_assoc()){
-			$output .= '<div class="item" style="text-align: center; margin-bottom: 10px;">
-				<img style="height: 120px; width:80px; border-radius:4px;" src="assets/book_covers/'.$row['product_image'].'">
-				<h4 style="font-size: 14px;"><i class="fas fa-book"></i>  '.$row['product_name'].'</h4>
-				<h4  style="font-size: 13px;"><i class="fas fa-dollar-sign"></i> '.$row['price'].' </h4>
-				<button type="button" class="cart-delete-btn" onclick="delete_order('.$row['id'].')"> <i class="fas fa-trash-alt"></i> </button>
+				<h4  style="font-size: 13px; display: inline;"><i class="fas fa-dollar-sign"></i> '.$row['price'].' </h4>
+				<button type="button" style="font-size: 14px;" class="cart-delete-btn" onclick="delete_cart('.$row['id'].')"> <i class="fas fa-trash-alt"></i> </button>
 			</div>';
 		}
 		print_r($output);
@@ -45,41 +26,45 @@
 	if(isset($_GET['delete_cart'])){
 		$result = $cart->delete_cart($_GET['delete_cart']);
 	}
-	if(isset($_GET['delete_order'])){
-		$result = $cart->delete_order($_GET['delete_order']);
-	}
-	if(isset($_GET['cart_to_order'])){
-		$cart->cart_to_order($_GET['cart_to_order'], $_GET['user']);
-	}
 	if(isset($_GET['count_order'])){
 		$count = $cart->count_order();
 		$row = $count->fetch_assoc();
 		print_r($row['COUNT(*)']);
 	}
+	if(isset($_GET['admin_delete_all'])){
+		$cart->delete_all();
+	}
+	if(isset($_GET['get_cart_info'])){
+		$result = $cart->get_cart($_GET['get_cart_info']);
+		$total_item = 0;
+		$total_price = 0;
+		while($row = $result->fetch_assoc()){
+			$total_item += 1;
+			$total_price += $row['price'];
+		}
+		$data = array(
+			'total_price' => $total_price,
+			'total_item' => $total_item
+		);
+		$payload = json_encode(array("data" => $data));
+		print_r($payload);
+	}
+	if(isset($_POST['address'])){
+		$result = $cart->place_order();
+		$_SESSION['msg'] = $result;
+		header("Location: ../index.html");
+	}
 	if(isset($_GET['admin_order'])){
 		$result = $cart->get_order();
-		$output = '';
-		$i = 1;
-		while($row = $result->fetch_assoc()){
-			$user_result = $user->get_user($row['user_id']);
-			$user_row = $user_result->fetch_assoc();
-			$output .= '
-				<tr>
-					<td> '.$i.'</td>
-					<td> '.$row['product_name'].' </td>
-					<td> '.$user_row['name'].' </td>
-					<td> '.$user_row['email'].' </td>
-					<td> '.$row['price'].' </td>
-					<td class="delete-td" onclick="delete_order('.$row['id'].')"><i class="far fa-trash-alt"></i></td>
-				</tr>
-			';
-			$i++;
-		}
-		print_r($output);
+		print_r($result);
 	}
 	if(isset($_GET['admin_order_delete'])){
 		$cart->delete_order($_GET['admin_order_delete']);
 	}
 	if(isset($_GET['admin_delete_all'])){
-		$cart->delete_all();
+		$cart->delete_order();
+	}
+	if(isset($_GET['admin_order_info'])){
+		$result = $cart->get_orderinfo($_GET['admin_order_info']);
+		print_r($result);
 	}
