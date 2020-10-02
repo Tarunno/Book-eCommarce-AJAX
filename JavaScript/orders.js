ce6f1a0e3150b585a085e9a1a981141ff97b9bde
Mahadi Hasan Tarunno <47926003+Tarunno@users.noreply.github.com>
2020-10-03 01:01:10 +0600

    Update orders.js

--- JavaScript/orders.js
+++ JavaScript/orders.js
@@ -24,8 +24,44 @@ function delete_all(){
 	xhr.open('GET', '../includes/cart.inc.php?admin_delete_all', true);
 	xhr.onload = function(){
 		if(this.status == 200){
-			get_user();
+			get_order();
 		}
 	}
 	xhr.send();
+}
+function get_orderinfo(id){
+	let body = document.body;
+	var item_container = document.createElement('div');
+	var item_window = document.createElement('div');
+	var item_info = document.createElement('div');
+	item_window.setAttribute('class', 'full-view');
+	item_info.setAttribute('class', 'about-text');
+	item_container.setAttribute('class', 'full-view-container');
+	item_info.innerHTML = `
+			<button class="full-view-off"><i class="fas fa-times"></i></button>
+			<h1> Order details </h1>
+			<div class="order-res">
+			</div>
+			`;
+	body.appendChild(item_container);
+	item_container.appendChild(item_window);
+	item_window.appendChild(item_info);
+
+	full_view_off_cart();
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', '../includes/cart.inc.php?admin_order_info='+id, true);
+	xhr.onload = function(){
+		if(this.status == 200){
+			document.querySelector('.order-res').innerHTML = this.responseText;
+		}
+	}
+	xhr.send();
+}
+function full_view_off_cart(){
+	let body = document.querySelector('.full-view-off');
+	let item = document.querySelector('.full-view-container');
+	body.addEventListener('click', function(){
+		item.remove();
+	});
 }
